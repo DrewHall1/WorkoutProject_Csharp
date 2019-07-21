6e8458a53f8903c50a4c79e32e86ed6159c6b37b
Drew Hall <drew.hall.usa@outlook.com>
2019-07-21 15:13:19 -0400

    initial commit

---
+++ NewMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace WorkoutProject_Csharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fileName = @"ListOfExercises.csv";
+
+            List<Workout> Exercises = new List<Workout>();
+
+            ReadWrite.ReadData(fileName, Exercises);
+
+            var Finished = false;
+
+            while(!Finished)
+            {
+                Console.WriteLine("Welcome to Basic Training you slimey maggot! ");
+                Console.WriteLine("Choose your entrance to hell on earth - 1. View all workouts 2. Make my own, 3. Edit existing, 4. I'm not worthy today - I QUIT!");
+
+                var userChoice = Console.ReadKey();
+                switch (userChoice.KeyChar)
+                {
+                    case '1':
+                        ReadWrite.ViewData(Exercises);
+                        break;
+
+                    case '2':
+                        ReadWrite.AddWorkout(Exercises);
+                        break;
+
+                    case '3':
+                        ReadWrite.EditWorkout(Exercises, fileName);
+                        break;
+
+                    case '4':
+                        Finished = true;
+                        break;
+                }
+
+                ReadWrite.WriteData(fileName, ListOfExercises);
+    }
+}
---
+++ ReadWrite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+
+namespace WorkoutProject_Csharp
+{
+    public class ReadWrite
+    {
+        public static List<Workout> ReadData(string fileName, List<Workout>)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                while(!reader.EndOfStream)
+                {
+                    var row = reader.ReadLine();
+                    var column = row.Split(',');
+                    var Workout = new Workout()
+                    {
+                        Id = column[0],
+                        Type = column[1],
+                        Name = column[2]
+                    };
+                    Exercises.Add(Workout);
+                }
+            }
+            return Exercises;
+        }
+    }
+}
